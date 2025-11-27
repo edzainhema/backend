f88327ff0360872ef696ec916357cbea632faad6
Edzai Nhema <nhemaed@gmail.com>
2025-11-27 08:27:13 -0400

    Made indentation adjustments

--- api/serializers.py
+++ api/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from .models import Media
 
 class MediaSerializer(serializers.ModelSerializer):
-    file_url = serializers.SerializerMethodField()
+	file_url = serializers.SerializerMethodField()
+	
 	class Meta:
 		model = Media
 		fields = ['id', 'user', 'file', 'file_url', 'uploaded_at']
 	
-	def get_file_url(self, obj):
-		request = self.context.get('request')
-		if request:
-			return request.build_absolute_uri(obj.file.url)
-		return obj.file.url
+		def get_file_url(self, obj):
+			request = self.context.get('request')
+			if request:
+				return request.build_absolute_uri(obj.file.url)
+			return obj.file.url
+	
